480d0185c06d0b7ff57d669896609533617190f8
Juliana Zerbinatti Sendon Garrido <juliana.garrido@edu.senai.br>
2026-05-14 17:42:45 +0000

    Limpeza: mantendo apenas a pasta dartpedia

---
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -0,0 +1,42 @@
+/*-----------------------------------------------------------------------------
+
+Versao: 0.0.0
+
+Data: 11/05/2026
+
+Descricao do codigo: Abra o arquivo comando_runner/lib/src/comando_runner_base.dart.O.
+
+Remova qualquer código de espaço reservado existente e adicione o seguinte Runner de comando classe para comando_runner/lib/src/comando_runner_base.dart¡::
+
+classe Runner de comando {
+  /// Executa a lógica de aplicação da linha de comando com os argumentos dados.
+  Futuro<vazio› › correr((S)List<Barbante› › entrada(S) assíncrono {
+    imprimir((S)´ 'CommandRunner recebeu argumentos: $entrada´ '(S)¡;
+  O}
+O}
+Destaques do código anterior:
+
+Runner de comando é uma classe que serve como um stand-in simplificado por enquanto. O ITS correr método atualmente apenas imprime os argumentos que recebe. Em capítulos posteriores, você expandirá essa classe para lidar com análise de comandos complexos e configuráveis.
+Futuro<void> é um tipo de retorno que indica que este método pode realizar operações assíncronas, mas não retorna um valor.
+
+Codigo:
+
+
+// TODO: Put public facing types in this file.
+
+/// Checks if you are awesome. Spoiler: you are.
+class Awesome {
+  bool get isAwesome => true;
+}
+
+-------------------------------------------------------------------------------
+
+Codigo valido ate aqui:
+*/
+
+classe Runner de comando {
+  /// Executa a lógica de aplicação da linha de comando com os argumentos dados.
+  Futuro<vazio› › correr((S)List<Barbante› › entrada(S) assíncrono {
+    imprimir((S)´ 'CommandRunner recebeu argumentos: $entrada´ '(S)¡;
+  O}
+O}
