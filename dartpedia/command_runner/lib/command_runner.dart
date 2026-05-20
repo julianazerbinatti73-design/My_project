0840a68bd363bdc379e06b14b59c9189801aab30
Maely Peixoto Santos <maely.santos@edu.senai.br>
2026-05-20 02:08:11 +0000

    Licão 5: Defina as relações com as classese. Tarefa 2: Atualizar a classe CommandRunner. Eu fiz toda a tarefa 2

--- dartpedia/command_runner/lib/command_runner.dart
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -1,86 +1,134 @@
 /*-----------------------------------------------------------------------------
 
-//Informacoes:
+Informacoes:
 
-//O pacote command_runner faz parte do projeto Dartpedia.
+O pacote command_runner faz parte do projeto Dartpedia.
 
-//Este arquivo funciona como a interface principal do pacote, definindo o queß sera exposto para o aplicativo CLI. command_runner.dart - Fica localizado na pasta dartpedia, em /dartpedia/command_runner/lib/command_runner.dart
+Este arquivo funciona como a interface principal do pacote, definindo o queß sera exposto para o aplicativo CLI. command_runner.dart - Fica localizado na pasta dartpedia, em /dartpedia/command_runner/lib/command_runner.dart
 
-//Outras informacoes importantes sobre o command_runner; nosso pacote:
+Outras informacoes importantes sobre o command_runner; nosso pacote:
 
-//site            :
+site            :
 
-//Autor           : Juliana Zerbinatti Sendon Garrido <julianazerbinatti73@gmail.com>
+Autor           : Juliana Zerbinatti Sendon Garrido <julianazerbinatti73@gmail.com>
 
-//Manutencao      :
+Manutencao      :
 
-//-----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Funcionamento:
+Funcionamento:
 
-//Este pacote define a estrutura de comandos. Ele exporta a classe CommandRunner para que ela possa ser utilizada por outras partes do projeto.
+Este pacote define a estrutura de comandos. Ele exporta a classe CommandRunner para que ela possa ser utilizada por outras partes do projeto.
 
-//Dentro da pasta dartpedia em /dartpedia/command_runner, rode o comando dart pub get para instalar e organizar as dependencias internas do pacote.
+Dentro da pasta dartpedia em /dartpedia/command_runner, rode o comando dart pub get para instalar e organizar as dependencias internas do pacote.
 
-//Obs. Importante ! Este pacote eh uma biblioteca modular e seu comportamento sera expandido nos proximos capitulos para lidar com comandos complexos.
+Obs. Importante ! Este pacote eh uma biblioteca modular e seu comportamento sera expandido nos proximos capitulos para lidar com comandos complexos.
 
-//-----------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
-//Dicionario do versionamento:
+Dicionario do versionamento:
 
-//v = versao
+v = versao
 
-//0 = Inicio do versionamento
+0 = Inicio do versionamento
 
-//. = Implementacao de versionamento
+. = Implementacao de versionamento
 
-//1 = Proxima versao, 2, proxima versao, e assim por diante.
+1 = Proxima versao, 2, proxima versao, e assim por diante.
 
-//-----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Historico de versionamento:
+Historico de versionamento:
 
-//----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Versao: 0.0.1
+Versao: 0.0.1
 
-//Data: 11/05/2026
+Data: 11/05/2026
 
-//Descricao do codigo:  Implemente a classe CommandRunner
+Descricao do codigo:  Implemente a classe CommandRunner
 
-//Agora que você criou o command_runner pacote, adicione uma classe provisória que eventualmente lidará com a lógica de análise dos argumentos da linha de comando.
+Agora que você criou o command_runner pacote, adicione uma classe provisória que eventualmente lidará com a lógica de análise dos argumentos da linha de comando.
 
-//1.Abra o command_runner/lib/command_runner.dart arquivo. Remova qualquer código de espaço reservado existente e adicione o seguinte:
+1.Abra o command_runner/lib/command_runner.dart arquivo. Remova qualquer código de espaço reservado existente e adicione o seguinte:
 
 /// A simple command runner to handle command-line arguments.
 ///
 /// More extensive documentation for this library goes here.
 //library;
 
-//export 'src/command_runner_base.dart';
-// TODO: Export any other libraries intended for clients of this package.
+export 'src/command_runner_base.dart';
+TODO: Export any other libraries intended for clients of this package.
 
-//Principais trechos do código anterior:
+Principais trechos do código anterior:
 
-//library; Declara este arquivo como uma biblioteca, que define os limites e a interface pública de uma unidade reutilizável de código Dart.
-//export 'src/command_runner_base.dart'; é uma linha crucial que torna as declarações de command_runner_base.dartdisponíveis para outros pacotes que importam o command_runnerpacote. Sem essa export declaração, as classes e funções dentro command_runner_base.dart seriam privadas ao command_runner pacote e você não conseguiria usá-las em sua dartpedia aplicação.
+library; Declara este arquivo como uma biblioteca, que define os limites e a interface pública de uma unidade reutilizável de código Dart.
+export 'src/command_runner_base.dart'; é uma linha crucial que torna as declarações de command_runner_base.dartdisponíveis para outros pacotes que importam o command_runnerpacote. Sem essa export declaração, as classes e funções dentro command_runner_base.dart seriam privadas ao command_runner pacote e você não conseguiria usá-las em sua dartpedia aplicação.
 
-//Codigo:
+Codigo:
 
 /// A simple command runner to handle command-line arguments.
 ///
 /// More extensive documentation for this library goes here.
-//library;  //Codigo; Ativar
+library; //Codigo; Ativar
 
-//export 'src/command_runner_base.dart'; //Codigo; Ativar
-// TODO: Export any other libraries intended for clients of this package.
+export 'src/command_runner_base.dart'; //Codigo; Ativar
+TODO: Export any other libraries intended for clients of this package.
 
-//-----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Codigo ate aqui:
-*/
-//Codigo:
 
+Versao: 0.0.2
+
+Data: 11/05/2026
+
+Descricao do codigo: Atualizar a classe CommandRunner
+
+Abra o arquivo command_runner/lib/command_runner.darte adicione as seguintes exportações:
+
+command_runner/lib/command_runner.dart
+
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
 library;
 
+export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
+export 'src/help_command.dart';
+
+// TODO: Export any libraries intended for clients of this package.
+
+Isso torna os arquivos arguments.dart, command_runner_base.dart, e help_command.dartdisponíveis para outros pacotes que dependem de command_runner. 
+
+Codigo: 
+
+
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/arguments.dart';
+export 'src/command_runner_base.dart';
+export 'src/help_command.dart';
+
+// TODO: Export any libraries intended for clients of this package.
+-------------------------------------------------------------------------------
+
+Codigo ate aqui : 
+*/
+
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/arguments.dart';
+export 'src/command_runner_base.dart';
+export 'src/help_command.dart';
+
+// TODO: Export any libraries intended for clients of this package.
+
+
+
--- dartpedia/command_runner/lib/src/command_runner_base.dart
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -1,52 +1,52 @@
-//-----------------------------------------------------------------------------
+/*-----------------------------------------------------------------------------
 
-//Informacoes:
+Informacoes:
 
-//Este arquivo contem a implementacao real (a logica) do pacote command_runner.
+Este arquivo contem a implementacao real (a logica) do pacote command_runner.
 
-//command_runner_base.dart - Localizado em /dartpedia/command_runner/lib/src/
+command_runner_base.dart - Localizado em /dartpedia/command_runner/lib/src/
 
-//Outras informacoes importantes sobre o pacote:
+Outras informacoes importantes sobre o pacote:
 
-//site            :
+site            :
 
-//E-mail                    : Juliana Zerbinatti Sendon Garrido <julianazerbinatti73@gmail.com>
+E-mail                    : Juliana Zerbinatti Sendon Garrido <julianazerbinatti73@gmail.com>
 
-//Manutencao      :
+Manutencao      :
 
-//-----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Funcionamento:
+Funcionamento:
 
-//Define a classe CommandRunner e o metodo run(), que e responsavel por receber e processar os comandos enviados pelo aplicativo cli.
+Define a classe CommandRunner e o metodo run(), que e responsavel por receber e processar os comandos enviados pelo aplicativo cli.
 
-//Obs. Importante ! Este motor eh simplificado por enquanto; a analise de comandos complexos sera integrada aqui em capitulos posteriores.
+Obs. Importante ! Este motor eh simplificado por enquanto; a analise de comandos complexos sera integrada aqui em capitulos posteriores.
 
-//-----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Dicionario do versionamento:
+Dicionario do versionamento:
 
-//v = versao
+v = versao
 
-//0 = Inicio do versionamento
+0 = Inicio do versionamento
 
-//. = Implementacao de versionamento
+. = Implementacao de versionamento
 
-//1 = Proxima versao, 2, proxima versao, e assim por diante.
+1 = Proxima versao, 2, proxima versao, e assim por diante.
 
-//-----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Historico de Versionamento:
+Historico de Versionamento:
 
-//-----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Versao: 0.0.0
+Versao: 0.0.0
 
-//Data:11/05/2026
+Data:11/05/2026
 
-//Descricao do codigo: Codigo original do pacote
+Descricao do codigo: Codigo original do pacote
 
-//Codigo:
+Codigo:
 
 // TODO: Put public facing types in this file.
 
@@ -55,43 +55,192 @@
 //  bool get isAwesome => true;    //Ativo
 //}   //Ativo
 
-//-----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-//Versao: 0.0.1
+Versao: 0.0.1
 
-//Data:11/05/2026
+Data:11/05/2026
 
-//Descricao do codigo: Remova qualquer código de espaço reservado existente e adicione a seguinte CommandRunner classe a command_runner/lib/src/command_runner_base.dart:
-
-//class CommandRunner {
-  /// Runs the command-line application logic with the given arguments.
-//  Future<void> run(List<String> input) async {
-//    print('CommandRunner received arguments: $input');
-//  }
-//}
-//Principais trechos do código anterior:
-
-//CommandRunner é uma classe que serve como substituta simplificada por enquanto. Seu runmétodo atualmente apenas imprime os argumentos recebidos. Em capítulos posteriores, você expandirá essa classe para lidar com a análise de comandos complexos e configuráveis.
-//Future<void> é um tipo de retorno que indica que este método pode executar operações assíncronas, mas não retorna um valor.
-
-//Codigo:
-
-//class CommandRunner {
-  /// Runs the command-line application logic with the given arguments.
-//  Future<void> run(List<String> input) async {
-//    print('CommandRunner received arguments: $input');
-//  }
-//}  
-
-//-----------------------------------------------------------------------------
-
-//Codigo ate aqui:
-
-//Codigo:
+Descricao do codigo: Remova qualquer código de espaço reservado existente e adicione a seguinte CommandRunner classe a command_runner/lib/src/command_runner_base.dart:
 
 class CommandRunner {
-
- Future<void> run(List<String> input) async {
-    print('CommandRunner received arguments: $input');
+ Runs the command-line application logic with the given arguments.
+  Future<void> run(List<String> input) async {
+  print('CommandRunner received arguments: $input');
   }
 }
+  Principais trechos do código anterior:
+
+CommandRunner é uma classe que serve como substituta simplificada por enquanto. Seu runmétodo atualmente apenas imprime os argumentos recebidos. Em capítulos posteriores, você expandirá essa classe para lidar com a análise de comandos complexos e configuráveis.
+Future<void> é um tipo de retorno que indica que este método pode executar operações assíncronas, mas não retorna um valor.
+
+Codigo:
+
+  class CommandRunner {
+      Runs the command-line application logic with the given arguments.
+    Future<void> run(List<String> input) async {                                                                                      print('CommandRunner received arguments: $input');
+  }
+}  
+
+-------------------------------------------------------------------------------
+
+Codigo ate aqui:
+
+Version 0.0.2
+
+Data : 19/05/2026
+
+Descricao do codigo : Atualizar a classe CommandRunner
+
+Substitua a CommandRunner classe existente pela seguinte:
+
+command_runner/lib/src/command_runner_base.dart
+
+import 'dart:collection';
+import 'dart:io';
+import 'arguments.dart';
+
+class CommandRunner {
+  final Map<String, Command> _commands = <String, Command>{};
+
+ UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  Future<void> run(List<String> input) async {
+  final ArgResults results = parse(input);
+    if (results.command != null) {
+       Object? output = await results.command!.run(results);
+      print(output.toString());
+    }
+  }
+
+    void addCommand(Command command) {
+       TODO: handle error (Commands can't have names that conflict)
+     _commands[command.name] = command;
+     command.runner = this;
+  }
+
+   ArgResults parse(List<String> input) {
+    var results = ArgResults();
+    results.command = _commands[input.first];
+    return results;
+  }
+
+  // Returns usage for the executable only.
+  // Should be overridden if you aren't using [HelpCommand]
+  // or another means of printing usage.
+
+  String get usage {
+   final exeFile = Platform.script.path.split('/').last;
+   return 'Usage: dart bin/$exeFile <command> [commandArg?] [...options?]';
+    }
+ }
+
+Esta classe atualizada incorpora sua nova estrutura orientada a objetos. Ela usa o operador de propagação (spread operator ...)no commands getter para desempacotar os valores do _commands mapa em um novo conjunto. Isso garante que o valor de retorno seja uma cópia, impedindo que código externo modifique seus dados.
+
+No run() método, results.command!.run(...) usa-se o operador de asserção não nulo ( !) para dizer ao analisador Dart que você tem certeza results.command de que não é nulo. É seguro aqui porque você acabou de verificar se não era nulo na if instrução anterior.
+
+Aqui estão os principais detalhes de implementação:
+
+°_commands mapa: Um mapa privado que associa nomes de comandos às suas Command instâncias de objetos concretos. Seus valores sao expostos por meio de um UnmodifiableSetView. °addCommand() Registra um comando e atribui this o executor à runner propriedadedo comando. Isso cumpre a late promessa de inicialização feita anteriormente na Command classe.
+
+°parse() e run(): Avalia a entrada do usuário, identifica o correspondente Command no mapa e usa await para chamar o método do comando run().
+
+
+Codigo:
+
+import 'dart:collection';
+import 'dart:io';
+import 'arguments.dart';
+
+class CommandRunner {
+  final Map<String, Command> _commands = <String, Command>{};
+
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  Future<void> run(List<String> input) async {
+    final ArgResults results = parse(input);
+    if (results.command != null) {
+      Object? output = await results.command!.run(results);
+      print(output.toString());
+    }
+  }
+
+  void addCommand(Command command) {
+    // TODO: handle error (Commands can't have names that conflict)
+    _commands[command.name] = command;
+    command.runner = this;
+  }
+
+  ArgResults parse(List<String> input) {
+    var results = ArgResults();
+    results.command = _commands[input.first];
+    return results;
+  }
+
+  // Returns usage for the executable only.
+  // Should be overridden if you aren't using [HelpCommand]
+  // or another means of printing usage.
+
+  String get usage {
+    final exeFile = Platform.script.path.split('/').last;
+    return 'Usage: dart bin/$exeFile <command> [commandArg?] [...options?]';
+  }
+}
+
+-------------------------------------------------------------------------------
+
+Codigo ate aqui : 
+
+Codigo: 
+
+*/
+
+import 'dart:collection';
+import 'dart:io';
+import 'arguments.dart';
+
+class CommandRunner {
+  final Map<String, Command> _commands = <String, Command>{};
+
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+class CommandRunner {
+  final Map<String, Command> _commands = <String, Command>{};
+
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  Future<void> run(List<String> input) async {
+    final ArgResults results = parse(input);
+    if (results.command != null) {
+      Object? output = await results.command!.run(results);
+      print(output.toString());
+    }
+  }
+
+  void addCommand(Command command) {
+    // TODO: handle error (Commands can't have names that conflict)
+    _commands[command.name] = command;
+    command.runner = this;
+  }
+
+  ArgResults parse(List<String> input) {
+    var results = ArgResults();
+    results.command = _commands[input.first];
+    return results;
+  }
+
+  // Returns usage for the executable only.
+  // Should be overridden if you aren't using [HelpCommand]
+  // or another means of printing usage.
+
+  String get usage {
+    final exeFile = Platform.script.path.split('/').last;
+    return 'Usage: dart bin/$exeFile <command> [commandArg?] [...options?]';
+  }
+} 
+
+
